08d3288bfdce5d166670f8a8af60333fb251a25b
Samuel Moreno Vahos <95778014+Samuel0104@users.noreply.github.com>
2022-05-15 19:46:00 -0500

    Update ted_dml.sql

--- ted_talks/ted_dml.sql
+++ ted_talks/ted_dml.sql
@@ -1,41 +1,41 @@
 COPY idioma(identificador, nombre)
-	FROM 'C:\segunda_entrega\datasets\idioma.csv'
+	FROM 'C:\datasets\idioma.csv'
 		WITH DELIMITER ';'
 		CSV HEADER;
 
 COPY patrocinador(identificador, nombre)
-	FROM 'C:\segunda_entrega\datasets\patrocinador.csv'
+	FROM 'C:\datasets\patrocinador.csv'
 		WITH DELIMITER ';'
 		CSV HEADER;
 
 COPY charla(identificador, titulo, descripcion, identificador_idioma, identificador_patrocinador)
-	FROM 'C:\segunda_entrega\datasets\charla.csv'
+	FROM 'C:\datasets\charla.csv'
 		WITH DELIMITER ';'
 		CSV HEADER
 		ENCODING 'windows-1251';
 
 COPY video(identificador, visitas, traducido, destacado, duracion, contexto, hora, dia, mes, anio, identificador_charla)
-	FROM 'C:\segunda_entrega\datasets\video.csv'
+	FROM 'C:\datasets\video.csv'
 		WITH DELIMITER ';'
 		CSV HEADER
 		ENCODING 'windows-1251';
 
 COPY tema(identificador, nombre)
-	FROM 'C:\segunda_entrega\datasets\tema.csv'
+	FROM 'C:\datasets\tema.csv'
 		WITH DELIMITER ';'
 		CSV HEADER;
 
 COPY ponente(identificador, nombre, apellido, ha_publicado)
-	FROM 'C:\segunda_entrega\datasets\ponente.csv'
+	FROM 'C:\datasets\ponente.csv'
 		WITH DELIMITER ';'
 		CSV HEADER;
 
 COPY charla_tema(identificador_charla, identificador_tema)
-	FROM 'C:\segunda_entrega\datasets\charla_tema.csv'
+	FROM 'C:\datasets\charla_tema.csv'
 		WITH DELIMITER ';'
 		CSV HEADER;
 
 COPY charla_ponente(identificador_charla, identificador_ponente)
-	FROM 'C:\segunda_entrega\datasets\charla_ponente.csv'
+	FROM 'C:\datasets\charla_ponente.csv'
 		WITH DELIMITER ';'
 		CSV HEADER;
